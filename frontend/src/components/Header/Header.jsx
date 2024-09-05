e392d34eb98ddf43aa9ea0e348173d7f1039530c
Aashish Pandey <pandey786aashish@gmail.com>
2024-09-05 10:27:51 +0530

    creating Navbar section

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+          Hii aashish Pandey
+        </div>
+    </div>
+  )
+}
+
+export default Header
