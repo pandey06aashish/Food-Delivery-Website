b56f2890da72e8041398d42e5ddf03a17c9d7ea3
Aashish Pandey <pandey786aashish@gmail.com>
2024-09-10 10:28:52 +0530

    create Place order page and total Amount calculation functionality

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -25,9 +25,16 @@ const StoreContextProvider = (props) => {
         });
     };
 
-    useEffect(() => {
-        console.log(cartItems);
-    }, [cartItems]);
+     const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo=food_list.find((product)=>product._id===item);
+                totalAmount+=itemInfo.price*cartItems[item];
+            }
+        }
+        return totalAmount;
+     }
 
     const contextValue = {
         food_list,
@@ -35,6 +42,7 @@ const StoreContextProvider = (props) => {
         cartItems,
         addToCart,
         removeFromCart,
+        getTotalCartAmount
     };
 
     return (
